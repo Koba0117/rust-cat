9973821179ee773becabaae827939784f1e46099
Kobayashi Kyosuke <kyo0117dragonquest@gmail.com>
2024-07-26 06:29:23 +0900

    added file reading

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
-use std::io;
+use std::{
+    env, fs,
+    io::{self, BufRead},
+};
 
 fn main() -> io::Result<()> {
-    let stdin = io::stdin();
-    let buffer = stdin.lines();
-    buffer.flatten().for_each(|line| println!("{line}"));
+    let args: Vec<String> = env::args().collect();
+
+    if let Some(file_path) = args.get(1) {
+        let file = fs::File::open(file_path)?;
+        let lines = io::BufReader::new(file).lines();
+        lines.flatten().for_each(|x| println!("{x}"));
+    } else {
+        let stdin = io::stdin();
+        let lines = stdin.lines();
+        lines.flatten().for_each(|x| println!("{x}"));
+    }
+
     Ok(())
 }
